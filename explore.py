d351d17c0469928fe39169b2a07b752cfddeebe0
ATufekovic <anto.tufekovic@gmail.com>
2020-06-15 17:54:27 +0200

    changed the csv source file because of an error, added identifiers to html elements, decided on a final JS library to use

--- explore.py
+++ explore.py
@@ -3,3 +3,5 @@ import pandas as pd
 
 csv = pd.read_csv("./smol.csv", ";")
 print("CSV file has " + str(len(csv.index)) + " rows")
+print(csv.head())
+csv.to_csv("test.csv", index=False, header=True)
